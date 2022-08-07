ed9d59c47cd6592cf9d53ba756c55c3bec42119f
Tatyana <tawita@yandex.ru>
2022-08-07 22:52:03 +0500

    Решили задачу 8

---
+++ zadacha4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+for (int numberB = 1; numberB <= numberA; numberB ++)
+if(numberB % 2 == 0)
+    Console.WriteLine(numberB);
+
+   
