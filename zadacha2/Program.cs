6f3e737198a2cd9d24a5085a3cfd4667defe93cc
Tatyana <tawita@yandex.ru>
2022-08-07 20:22:06 +0500

    Решили задачу 4

---
+++ zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+
+ Console.WriteLine(max);
+
