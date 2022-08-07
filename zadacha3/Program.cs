9256398004c49862d0618fd299b48956ddbca9eb
Tatyana <tawita@yandex.ru>
2022-08-07 22:20:13 +0500

    Решили задачу 6

---
+++ zadacha3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+int numberB = numberA % 2;
+
+bool result = numberB == 0;
+
+if(result)
+{
+    Console.WriteLine("четное число");
+}
+   
+else
+{
+    Console.WriteLine("нечетное число");
+}
