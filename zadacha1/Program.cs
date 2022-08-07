77f2accd25c241812d99d2967fa8e15d69f14cb9
Tatyana <tawita@yandex.ru>
2022-08-07 20:01:17 +0500

    Решили задачу 2

---
+++ zadacha1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int max = numberA;
+int min = numberB;
+
+if(numberB > max) max = numberB;
+
+ Console.WriteLine(max);
